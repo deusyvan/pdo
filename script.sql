02d658dbc6fc24d55a12a2cf8d122e0cecdd6761
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-22 10:17:43 -0300

    Conexão com o banco

---
+++ script.sql
@@ -0,0 +1,18 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: 22-Maio-2018 às 10:12
+-- Versão do servidor: 10.1.26-MariaDB-0+deb9u1
+-- PHP Version: 7.0.27-0+deb9u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `blog`
+--
+DROP DATABASE IF EXISTS `blog`;
+CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `blog`;

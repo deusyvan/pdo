b0aa95bf9d6374c26cd900b3f52d0944253f44c2
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-22 10:54:10 -0300

    Inserindo usuário e retornando id

--- index.php
+++ index.php
@@ -9,15 +9,15 @@ $dbpass = "admin";
 try {
     $pdo = new PDO($dsn, $dbuser, $dbpass);
     
-    $nome = "Testador";
-    $email = "teste@hotmail.com";
+    $nome = "Testador 2";
+    $email = "teste2@hotmail.com";
     $senha = md5("123");
     
     $sql = "INSERT INTO usuarios SET nome='$nome', email='$email', senha = '$senha'";
     
     $sql = $pdo->query($sql);//Retorna o id inserido.
     
-    echo "Usuário inserido com sucesso!";
+    echo "Usuário inserido com sucesso! Id= ".$pdo->lastInsertId();
    
         
     

9371b6b625040e61a2031de5457c6cd6a4fe42cb
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-22 10:32:51 -0300

    Script de banco

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 22-Maio-2018 às 10:12
+-- Generation Time: 22-Maio-2018 às 10:31
 -- Versão do servidor: 10.1.26-MariaDB-0+deb9u1
 -- PHP Version: 7.0.27-0+deb9u1
 
@@ -13,6 +13,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `blog`
 --
-DROP DATABASE IF EXISTS `blog`;
 CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `blog`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `senha` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'Boniek', 'suport@b7web.com.br', '21232f297a57a5a743894a0e4a801fc3'),
+(2, 'Fulano', 'fulano@hotmail.com', '21232f297a57a5a743894a0e4a801fc3'),
+(3, 'Cicrano', 'cicrano@gmail.com', '21232f297a57a5a743894a0e4a801fc3'),
+(4, 'Beltrano', 'beltrano@yahoo.com.br', '21232f297a57a5a743894a0e4a801fc3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
